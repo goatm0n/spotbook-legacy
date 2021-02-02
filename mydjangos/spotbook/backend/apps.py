97c7418083dcb7a56994f8ed1b7e2d5ddd307440
COMMUNISM <78227361+MrMao10@users.noreply.github.com>
2021-02-02 12:15:37 +0000

    Update apps.py
    link to my pokemon

--- mydjangos/spotbook/backend/apps.py
+++ mydjangos/spotbook/backend/apps.py
@@ -3,3 +3,15 @@ from django.apps import AppConfig
 
 class BackendConfig(AppConfig):
     name = 'backend'
+
+link = input('Do you want to go to Pokemon?')
+ if link = ('Yes')
+    print ('github.com/MrMao10/Pokemon')
+ elif link = ('No')
+   print('Ok')
+ elif link = ('yes')
+  print ('github.com/MrMao10/Pokemon')
+ elif link = ('no')
+     print ('Ok')
+    
+#tell me if this would work and if it dosn't improve it
