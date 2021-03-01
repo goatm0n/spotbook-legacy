6ae6492a71dca1f1b8b8232805cf277ce0525737
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2021-03-01 12:10:56 +0000

    spot create api update

--- mydjangos/spotbook/spots/api/urls.py
+++ mydjangos/spotbook/spots/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
     path('spot-list/', views.spotList, name='api-spot-list'),
     path('spot-detail/<str:pk>/', views.spotDetail, name='api-spot-detail'),
-    path('spot-create/', views.spotCreate, name='api-spot-create'),
+    path('spot-create/', views.spot_create, name='api-spot-create'),
 ]
