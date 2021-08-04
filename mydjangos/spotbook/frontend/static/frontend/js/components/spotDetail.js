7429a8ee183e86f2dafd2002e49332346aaafc41
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2021-08-04 23:27:42 +0100

    Update spotDetail.js

--- mydjangos/spotbook/frontend/static/frontend/js/components/spotDetail.js
+++ mydjangos/spotbook/frontend/static/frontend/js/components/spotDetail.js
@@ -14,46 +14,6 @@ function getCookie(name) {
     return cookieValue;
 }
 
-/* function renderSpotLikeButton(spot) {
-    const spotLikeButtonContainer = document.createElement('div');
-    spotLikeButtonContainer.id = 'spot-like-button-container';
-    
-    const spotLikeForm = document.createElement('form');
-    spotLikeForm.action = `http://127.0.0.1:8000/spots/api/spot-like/${spot.id}/`;
-    spotLikeForm.method = 'POST';
-
-    const spotLikeButton = document.createElement('button');
-    spotLikeButton.id = 'spot-like-button';
-    spotLikeButton.type = 'submit';
-    spotLikeButton.setAttribute('class', 'btn btn-secondary');
-    spotLikeButton.textContent = 'Like/ unlike';
-    spotLikeForm.appendChild(spotLikeButton);
-
-    const csrfInput = document.createElement('input');
-    const csrftoken = getCookie('csrftoken');
-    csrfInput.type = 'hidden';
-    csrfInput.name = 'csrfmiddlewaretoken';
-    csrfInput.value = csrftoken;
-    spotLikeForm.appendChild(csrfInput);
-    
-    spotLikeButtonContainer.appendChild(spotLikeForm);
-    
-    return spotLikeButtonContainer;
-} */
-
-/* function renderSpotLikeCounter(spot) {
-    const spotLikeCounterContainer = document.createElement('div');
-    spotLikeCounterContainer.id = 'spot-like-counter-container';
-    spotLikeCounterContainer.setAttribute('class', 'container');
-    const spotLikeCounter = document.createElement('p');
-    spotLikeCounter.id ='spot-like-counter';
-    spotLikeCounter.textContent = `${spot.properties.likes.length}`;
-    spotLikeCounterContainer.appendChild(spotLikeCounter);
-    
-    return spotLikeCounterContainer;
-
-} */
-
 function renderSpotDetail(spot) {
 
     const spotDetailContainer = document.createElement('div');
@@ -80,19 +40,9 @@ function renderSpotDetail(spot) {
     spotDescription.textContent = spot.properties.description;
     spotDescriptionDiv.appendChild(spotDescription);
 
-    /* const spotLikesDiv = document.createElement('div');
-    spotLikesDiv.id = 'spot-likes-container';
-    const spotLikeButton = renderSpotLikeButton(spot);
-    spotLikesDiv.appendChild(spotLikeButton);
-    const spotLikeCounter = renderSpotLikeCounter(spot);
-    spotLikesDiv.appendChild(spotLikeCounter); */
-    
-    
-
     spotDetailContainer.appendChild(spotTitleDiv);
     spotDetailContainer.appendChild(spotTypeDiv);
     spotDetailContainer.appendChild(spotDescriptionDiv);
-   /*  spotDetailContainer.appendChild(spotLikesDiv); */
 
     return spotDetailContainer;
 }
