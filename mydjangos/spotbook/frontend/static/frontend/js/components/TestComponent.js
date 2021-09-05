838717763c68ff51485a79155dc4be0022a34121
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2021-09-05 15:16:22 +0100

    dev

--- mydjangos/spotbook/frontend/static/frontend/js/components/TestComponent.js
+++ mydjangos/spotbook/frontend/static/frontend/js/components/TestComponent.js
@@ -1,9 +1,27 @@
-export class TestComponent {
-    constructor(id) {
-        this.id = id;
-    }
-    sayHi() {
-        console.log('hello');
-    }
-}
 
+
+// Component with virutal dom, click to increase number on button
+export default class TestComponent {
+    constructor(args) {
+        this.args = args;
+    }
+
+
+    renderDiv() {
+        var div = document.createElement('div');
+        div.id = 'test-component-div';
+
+        var targetDiv = document.getElementById(this.args.targetDiv);
+        
+        targetDiv.appendChild(div);
+        
+    }
+
+    renderVirtualDiv() {
+        var vDiv = document.createElement('div');
+        vDiv.id = 'virtual-test-component-div';
+    }
+
+    
+
+}
